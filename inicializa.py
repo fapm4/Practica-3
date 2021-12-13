37661b93b138f90a40d198904907a09a63bc2c36
Sherl0kk <68997742+Sherl0kk@users.noreply.github.com>
2021-12-13 17:16:15 +0100

    Add files via upload

---
+++ inicializa.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Practica 3
+
+# In[ ]:
+
+import pyodbc
+
+def createTables(conn):
+
+    try:
+        createClase = ''' CREATE TABLE CLASE( 
+            id_clase VARCHAR2(9),
+            tematica VARCHAR2(20), 
+            horario  DATE, 
+            CONSTRAINT PK_CLASE PRIMARY KEY(id_clase))
+        '''
+
+        with conn.cursor() as cursor: 
+            cursor.execute(createClase)
+            cursor.commit()
+
+        createEntrenadores = ''' CREATE TABLE ENTRENADORES(
+            DNI VARCHAR2(9),
+            NOMBRE VARCHAR2(20),
+            APELLIDOS VARCHAR2(20),
+            CORREO VARCHAR2(60),
+            DIRECCION VARCHAR2(60),
+            TELEFONO NUMBER,
+            ESPECIALIDAD VARCHAR2(30) CHECK (ESPECIALIDAD='Raqueta' OR ESPECIALIDAD='Equipo' OR ESPECIALIDAD='Personal') ,
+            SALARIO NUMBER DEFAULT 0,
+            CONSTRAINT PK_ENTRENADORES PRIMARY KEY (DNI),
+            CONSTRAINT UK_ENTRENADORES_CORREO UNIQUE (CORREO),
+            CONSTRAINT UK_ENTRENADORES_TELEFONO UNIQUE (TELEFONO))
+        '''
+
+        with conn.cursor() as cursor: 
+            cursor.execute(createEntrenadores)
+            cursor.commit()
+
+        createClientes = ''' CREATE TABLE CLIENTES(
+            DNI VARCHAR2(9),
+            NOMBRE VARCHAR2(20),
+            APELLIDOS VARCHAR2(20),
+            CORREO VARCHAR2(60),
+            DIRECCION VARCHAR2(20),
+            TELEFONO NUMBER,
+            TIPO_SUSCRIPCION VARCHAR2(2),
+            CONSTRAINT PK_CLIENTES PRIMARY KEY (DNI),
+            CONSTRAINT UK_CLIENTES_CORREO UNIQUE (CORREO),
+            CONSTRAINT UK_CLIENTES_TELEFONO UNIQUE (TELEFONO))
+        '''
+
+        with conn.cursor() as cursor: 
+            cursor.execute(createClientes)
+            cursor.commit()
+
+        createInstalacion = ''' CREATE TABLE INSTALACION(
+            id_instalacion VARCHAR2(9),
+            aforo NUMBER,
+            CONSTRAINT IPK_CLASE PRIMARY KEY(id_instalacion))
+        '''
+
+        with conn.cursor() as cursor: 
+            cursor.execute(createInstalacion)
+            cursor.commit()
+
+        createReserva = ''' CREATE TABLE RESERVA(
+            DNI VARCHAR2(9),
+            ID_INSTALACION VARCHAR2(9),
+            FECHA DATE,
+            CONSTRAINT PK_RESERVA PRIMARY KEY(DNI),
+            CONSTRAINT FK_RESERVA_INSTALACION FOREIGN KEY(ID_INSTALACION) REFERENCES INSTALACION)
+        '''
+
+        with conn.cursor() as cursor: 
+            cursor.execute(createReserva)
+            cursor.commit()
+        
+        createApuntado = ''' CREATE TABLE APUNTADO(
+            DNI VARCHAR2(9),
+            ID_CLASE VARCHAR2(9),
+            CONSTRAINT PK_APUNTADO PRIMARY KEY(DNI),
+            CONSTRAINT FK_APUNTADO_CLASE FOREIGN KEY(ID_CLASE) REFERENCES CLASE)
+        '''
+
+        with conn.cursor() as cursor: 
+            cursor.execute(createApuntado)
+            cursor.commit()
+
+        createImparte = ''' CREATE TABLE IMPARTE(
+            DNI VARCHAR2(9),
+            id_clase VARCHAR2(9),
+            CONSTRAINT EK_CLASEI FOREIGN KEY (id_clase) REFERENCES CLASE (id_clase) ON DELETE CASCADE,
+            CONSTRAINT EK_DNI FOREIGN KEY (DNI) REFERENCES ENTRENADORES (DNI) ON DELETE CASCADE,
+            CONSTRAINT PK_IMPARTE PRIMARY KEY(id_clase,DNI))
+        '''
+
+        with conn.cursor() as cursor: 
+            cursor.execute(createImparte)
+            cursor.commit()
+
+        createLugar = ''' CREATE TABLE LUGAR(
+            id_instalacion VARCHAR2(9),
+            id_clase VARCHAR2(9),
+            CONSTRAINT EK_CLASEL FOREIGN KEY (id_clase) REFERENCES CLASE,
+            CONSTRAINT EK_INSTALACION FOREIGN KEY (id_instalacion) REFERENCES INSTALACION,
+            CONSTRAINT PK_LUGAR PRIMARY KEY(id_clase,id_instalacion))
+        '''
+
+        with conn.cursor() as cursor: 
+            cursor.execute(createLugar)
+            cursor.commit()
+
+    except Exception as ex:
+        print(ex)
+
+#In [ ]:
+
+def dropBD(conn):
+    tablas=["LUGAR", "IMPARTE", "APUNTADO", "RESERVA", "INSTALACION", "CLIENTES", "ENTRENADORES", "CLASE"]
+    i=0
+    while i < 8:        
+        try:
+            vaciado = "DROP TABLE " + tablas[i]
+
+            with conn.cursor() as cursor:
+                cursor.execute(vaciado)
+                cursor.commit()
+
+        except Exception as ex:
+            print(ex)
+            
+            with conn.cursor() as cursor:
+                cursor.rollback()
+        i+=1
---
+++ main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Practica 3
+
+# In[ ]:
+
+import pyodbc
+import inicializa
+import subsistema_entrenadores
+import subsistema_clases
+
+import os
+
+from time import gmtime, strftime
+
+def conectaBase():
+    try:
+        conn = pyodbc.connect('DRIVER={Devart ODBC Driver for Oracle};Direct=True;Host=oracle0.ugr.es;Service Name=practbd.oracle0.ugr.es;User ID=x7390452;Password=x7390452')
+        conn.autocommit = False
+        cursor = conn.cursor()
+        
+    except Exception as ex:
+        print(ex)
+        
+    return conn
+
+def main():
+    conn = conectaBase()
+
+    print("Bienvenido al Centro Deportivo Epicardo.\nIndica a continuación una de las opciones.")
+    print('\n')
+    
+    val = 1
+
+    while 0 <= val <= 4:
+        print('0. Inicializar la base de datos\n')
+        print('1. Gestión de Clientes\n')
+        print('2. Gestión de Entrenadores\n')
+        print('3. Gestión de Clases\n')
+        print('4. Gestión de Instalaciones\n')
+        print('5. Salir\n')
+        print('Introduce opción: ')
+        val = int(input())
+        
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if val != 5:
+            if val == 0:
+                inicializa.dropBD(conn)
+                inicializa.createTables(conn)
+            elif val == 1:
+                print('Esta parte es de Pancho\n')
+            elif val == 2:
+                subsistema_entrenadores.gestionEntrenadores(conn)
+            elif val == 3:
+                subsistema_clases.gestionClases(conn)
+            elif val == 4:
+                print('Esta parte es de Fjorn\n')
+        else:
+            with conn.cursor() as cursor:
+                cursor.rollback()
+
+    conn.close()
+
+main()
